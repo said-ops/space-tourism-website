f20ef7e7e539ae77a5d32180401401d9cb0549e6
said-ops <idrissisaid29@gmail.com>
2024-07-22 17:03:21 +0000

    dest section/moon

--- script.js
+++ script.js
@@ -39,7 +39,149 @@ console.log(icon,menu);
         });
         //end menu event listener
 
-        
+        //==================destination section here==========================================================
+
+                                //==========================================================================
+                    //links emlementaion to change background image for every section and screen size
+                    document.querySelectorAll('li').forEach( li =>  {
+
+                        //change background four each link 
+
+                        switch (li.getAttribute('data-value')) {
+                            case 'destination':
+
+                                li.addEventListener('click' , () => {
+
+                                    if(window.innerWidth > 770){
+
+                                        document.body.style.background = 'url(./assets/destination/background-destination-desktop.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }else if(window.innerWidth < 580){
+
+                                        document.body.style.background = 'url(./assets/destination/background-destination-mobile.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }else if(window.innerWidth < 770 && window.innerWidth >580){
+
+                                        document.body.style.background = 'url(./assets/destination/background-destination-tablet.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }
+
+                                    
+                    
+                            });
+                                
+                                break;
+                            
+                             case 'home':
+                                 
+                                li.addEventListener('click' , () => {
+
+                                    if(window.innerWidth > 770){
+
+                                        document.body.style.background = 'url(./assets/home/background-home-desktop.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }else if(window.innerWidth < 580){
+
+                                        document.body.style.background = 'url(./assets/home/background-home-mobile.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }else if(window.innerWidth < 770 && window.innerWidth >580){
+
+                                        document.body.style.background = 'url(./assets/home/background-home-tablet.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }
+
+                            });
+
+                                break;
+
+                            case 'crew' :
+                                
+                                li.addEventListener('click' , () => {
+
+                                    if(window.innerWidth > 770){
+
+                                        document.body.style.background = 'url(./assets/crew/background-crew-desktop.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }else if(window.innerWidth < 580){
+
+                                        document.body.style.background = 'url(./assets/crew/background-crew-mobile.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }else if(window.innerWidth < 770 && window.innerWidth >580){
+
+                                        document.body.style.background = 'url(./assets/crew/background-crew-tablet.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+                                    }
+
+                            });
+
+                            break;
+
+                            case 'tech':
+
+                                li.addEventListener('click' , () => {
+
+                                     
+                                    
+                                    
+                                    if(window.innerWidth > 770){
+
+                                        document.body.style.background = 'url(./assets/technology/background-technology-desktop.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }else if(window.innerWidth < 580){
+
+                                        document.body.style.background = 'url(./assets/technology/background-technology-mobile.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }else if(window.innerWidth < 770 && window.innerWidth >580){
+
+                                        document.body.style.background = 'url(./assets/technology/background-technology-tablet.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }
+
+                                    
+
+                            });
+                              
+                            break;
+
+                            default:
+                                li.addEventListener('click' , () => {
+
+                                    if(window.innerWidth > 770){
+
+                                        document.body.style.background = 'url(./assets/home/background-home-desktop.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }else if(window.innerWidth < 580){
+
+                                        document.body.style.background = 'url(./assets/home/background-home-mobile.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }else if(window.innerWidth < 770 && window.innerWidth >580){
+
+                                        document.body.style.background = 'url(./assets/home/background-home-tablet.jpg)';
+                                        document.body.style.backgroundSize = 'cover';
+
+                                    }
+
+                            });
+                                break;
+                        }
+
+
+                    });
+
+
 
 
     });
